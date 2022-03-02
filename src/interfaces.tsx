c26119e707b6c88fa0a2761a8a7030e11b022e4b
Dave Martin <david.martin@tookay.net>
2022-03-02 19:30:17 +0000

    feat(interfaces.tsx): modularised TS interface

---
+++ src/interfaces.tsx
@@ -0,0 +1,13 @@
+export interface JSONdataType {
+  is_new_with_tag: boolean;
+  designer: string;
+  name: string;
+  size: string;
+  image: string;
+  price: number;
+  retail: number;
+  sizes: any;
+    // id: number;
+    // name: string;
+    // qty: number;
+}
